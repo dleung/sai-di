e44dea2d6b374dfb5102e21507f547d2e385ba50
David Leung <dleung@quosap.com>
2012-08-19 23:58:52 -0700

    more edits

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,17 @@
-set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+set :user, 'root'
+set :domain, 'capykoa.com'
+set :application, "capykoa"
+ 
+set :repository, "git@github.com:dleung/capykoa.com.git"  # Your clone URL
+set :scm, "git"
+set :branch, "master"
+set :scm_verbose, true
+set :scm_passphrase, "icecream"  # The deploy user's password
+set :deploy_to, "/home/#{user}/#{domain}"
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :normalize_asset_timestamps, false
+set :rvm_ruby_string, 'ruby-1.9.3-p194@capykoa'
 set :rvm_install_ruby_params, '--1.9'      # for jruby/rbx default to 1.9 mode
 
 before 'deploy:setup', 'rvm:install_rvm'   # install RVM
@@ -7,33 +20,14 @@ before 'deploy:setup', 'rvm:create_gemset' # only create gemset
 
 require "rvm/capistrano"
 
-set :user, 'root'
-set :domain, 'capykoa.com'
-set :application, "capykoa"
-set :repository,  "git@github.com:dleung/capykoa.com.git"
-set :branch, "master"
-set :scm, "git"
-set :scm_verbose, true
-set :deploy_via, :remote_cache
-set :scm_passphrase, "zero7one4"
-set :deploy_to, "/home/deploy/capykoa-web"
-set :use_sudo, false 
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-
-
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
+ 
+role :web, domain                         # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+ 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Capykoa::Application.initialize!
+passenger_user_switching off
