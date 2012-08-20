733c474468c09a8b3eda170c1d0a4442aec3e88c
David Leung <dleung@quosap.com>
2012-08-19 23:13:26 -0700

    fixing deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+set :rvm_install_ruby_params, '--1.9'      # for jruby/rbx default to 1.9 mode
+
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
+before 'deploy:setup', 'rvm:create_gemset' # only create gemset
+
+require "rvm/capistrano"
+
 set :user, 'root'
 set :domain, 'capykoa.com'
 set :application, "capykoa"
