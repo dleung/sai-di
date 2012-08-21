7c72aa9c157e3295e63d27f07d8272302b0000b3
David Leung <dleung@quosap.com>
2012-08-21 05:43:45 +0000

    fixed image path

--- db/migrate/20120816014519_devise_create_admins.rb
+++ db/migrate/20120816014519_devise_create_admins.rb
@@ -34,12 +34,15 @@ class DeviseCreateAdmins < ActiveRecord::Migration
       # t.string :authentication_token
       t.string :name
 
-
+      t.boolean :manual_verified, :boolean
       t.timestamps
       
     end
     add_index :admins, :email,                :unique => true
     add_index :admins, :reset_password_token, :unique => true
+    admin = Admin.create!( :email => 'davleun@gmail.com', :password => 'icecream', name: "David Leung")
+    admin.manual_verified = true
+    admin.save!
     # add_index :admins, :confirmation_token,   :unique => true
     # add_index :admins, :unlock_token,         :unique => true
     # add_index :admins, :authentication_token, :unique => true
