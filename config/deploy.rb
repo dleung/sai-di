1eab4e08cae68b72be955d2dbaa248e151ad532b
David Leung <dleung@quosap.com>
2012-08-23 04:22:39 +0000

    added bundler capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
 before 'deploy:setup', 'rvm:create_gemset' # only create gemset
 
 require "rvm/capistrano"
+require "bundler/capistrano"
 
 default_run_options[:pty] = true
  
@@ -31,18 +32,5 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
-
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
   end
 end
